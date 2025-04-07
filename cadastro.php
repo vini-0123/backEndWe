9d74fce64957526bcc5c1a78d048e2e40cefb684
RyanGBC <ryangabrj@gmail.com>
2025-04-07 16:54:09 -0300

    Fix: banco e php arrumado

--- cadastro.php
+++ cadastro.php
@@ -40,7 +40,7 @@ if (isset($_POST['email'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css"> 
         <!-- === IMPORTANT: Add Font Awesome === -->
